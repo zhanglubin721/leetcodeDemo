9fd9aa49ee093503eebd78b278d775cfc2122d92
zhanglubin <zhanglubin@teyuntong.com>
2023-02-23 17:23:03 +0800

    Initial commit

---
+++ src/main/java/com/zhanglubin/leetcodedemo/LeetcodeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zhanglubin.leetcodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhanglubin/leetcodedemo/LeetcodeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhanglubin.leetcodedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
