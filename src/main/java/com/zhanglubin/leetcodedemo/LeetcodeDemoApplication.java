28f14b42bef4e58e979650999253864dd69f7567
zhanglubin <zhanglubin@teyuntong.com>
2023-02-23 17:23:43 +0800

    1

--- src/main/java/com/zhanglubin/leetcodedemo/LeetcodeDemoApplication.java
+++ src/main/java/com/zhanglubin/leetcodedemo/LeetcodeDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LeetcodeDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LeetcodeDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeDemoApplication.class, args);
+    }
 
 }
